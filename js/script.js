f336f90e5ad642c7b4f16e6aa2e27c43e24ef9aa
Shogo Nameki <nametake.kyarabuki@gmail.com>
2014-03-02 18:48:48 +0900

    add parallax

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
 $(function () {
-    var w = $(window).width();
-    var h = $(window).height();
-    $('.slide').width(w);
-    $('.slide').height(h);
-
-    $('.slide').css('padding-top',h / 2);
-
-    $(window).resize(function() {
-        var w = $(window).width();
-        var h = $(window).height();
-        $('.slide').width(w);
-        $('.slide').height(h);
-        $('.slide').css('padding-top', h / 2);
-        $('html,body').animate({ scrollTop: 0 }, 'fast');
-        return false;
-    });
-
-    $('.slide').click(function () {
-        var i = $('.slide').index(this);
-        var p = $('.slide').eq(i+1).offset().top;
-        $('html,body').animate({ scrollTop: p }, 'fast');
-        return false;
-    });
+    //$(".left-in").animate({"left": "0%"}, {duration: 800, easing:'easeInQuad',});
+    //$("#test").animate({"left": "30%"}, {duration: 800, easing: 'easeInQuad',});
 })
