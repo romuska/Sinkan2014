07ce566ab81718198106099109b5dd0af7c9e4dc
Shogo Nameki <nametake.kyarabuki@gmail.com>
2014-03-03 15:04:37 +0900

    ソースをちょっと整理

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(function () {
-    //$(".left-in").animate({"left": "0%"}, {duration: 800, easing:'easeInQuad',});
+    //$("#pg2_txt1").animate({"left": "0%"}, {duration: 800, easing:'easeInQuad',});
     //$("#test").animate({"left": "30%"}, {duration: 800, easing: 'easeInQuad',});
+
 })
+//fullPage関係の設定
+$(document).ready(function() {
+    $.fn.fullpage({
+        slidesColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        onLeave: function(index, direction){
+            if(index == 1 && direction == 'down'){
+                $('#pg2_txt1').animate({'left': '50%'}, 1000);
+            }else if(index == 2 && direction == 'up'){
+                $('#pg2_txt1').animate({'left': '100%'}, 1000);
+            }
+        },
+    });
+});
