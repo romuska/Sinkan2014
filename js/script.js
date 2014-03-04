4ab295e63d3861041098918e13f2fc5fc602ffeb
Shogo Nameki <nametake.kyarabuki@gmail.com>
2014-03-04 15:46:12 +0900

    change color

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@ $(function () {
 //fullPage関係の設定
 $(document).ready(function() {
     $.fn.fullpage({
-        slidesColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        slidesColor: ['#cd9cc1', '#d3e3a2', '#fff9a2', '#ffd194', '#f9a383', '#f8a5a6'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
         onLeave: function(index, direction){
             if(index == 1 && direction == 'down'){
-                $('t1').animate({'left': '50%'}, 1000);
+                $('#pg2_txt1').animate({'left': '5%'}, 1000);
+                $('#pg2_txt2').animate({'left': '7%'}, 1200);
+                $('#pg2_txt3').animate({'left': '9%'}, 1400);
             }else if(index == 2 && direction == 'up'){
-                $('t1').animate({'left': '100%'}, 1000);
+                $('#pg2_txt1').animate({'left': '100%'}, 1400);
+                $('#pg2_txt2').animate({'left': '100%'}, 1200);
+                $('#pg2_txt3').animate({'left': '100%'}, 1000);
             }
         },
     });
